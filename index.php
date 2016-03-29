4a7f75d8cb7c8af79480eb7b225dae5e940157d2
Austin Nicholas <austin.nicholas@me.com>
2016-03-29 16:14:12 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head lang="en">
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>SpaceMate</title>
+
+</head>
+<body>
+
+  <header></header>
+  <section>
+    http://spacemate.xyz
+  </section>
+  <footer></footer>
+
+</body>
